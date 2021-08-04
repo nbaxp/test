a07574a4032e210f1b85785aef7282de6535e96d
wanggang <76527413@qq.com>
2021-08-04 08:50:56 +0800

    github actions docker buildx

--- build.cmd
+++ build.cmd
@@ -1,3 +1,2 @@
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker build --progress=plain -t 76527413/test:amd64 . 
-docker build --progress=plain -f Dockerfile.arm64 -t 76527413/test:arm64 . 
+docker buildx build . --platform linux/amd64 --tag 76527413/test:latest 
+docker buildx build . --platform linux/arm64 --tag 76527413/test:latest
--- start.cmd
+++
@@ -1,2 +0,0 @@
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker compose up -d
